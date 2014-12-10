ae6b9fd1ddd6611205df08c19e3ce4c7c97f8027
Dimitri Vranken <dimitri.vranken@hotmail.ch>
2014-12-10 22:45:10 +0100

    Minor HTML refactoring

--- end.php
+++ end.php
@@ -20,21 +20,17 @@
 
 <!DOCTYPE html>
 <html lang="de">
-
 <head>
     <title>Lea Berger | Umfrage</title>
-
+    
     <meta charset="utf-8">
-    <link rel="shortcut icon" type="image/x-icon" href="cubeicon.png">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-
-    <!-- Bootstrap -->
+    <link rel="shortcut icon" type="image/x-icon" href="cubeicon.png">
+    
+    <!-- Style -->
     <link href="//cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.0/paper/bootstrap.min.css" rel="stylesheet">
-
 </head>
-
 <body>
-
     <section>
         <div class="row">
             <div class="col-lg-12 text-center">
@@ -49,22 +45,19 @@
             </div>
         </div>
     </section>
-
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    
+    <footer class="footer">
+        <div class="container text-center">
+            <p class="text-muted">
+                Created by Nicola Bischof
+            </p>
+        </div>
+    </footer>
+    
+    <!-- Scripts -->
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.11.1/jquery.min.js" type="text/javascript"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js" type="text/javascript"></script>
-
+    
     <script type="text/javascript" src="game.js"></script>
-
 </body>
-
-<footer class="footer">
-    <div class="container text-center">
-        <p class="text-muted">
-            Created by Nicola Bischof
-        </p>
-    </div>
-</footer>
-
 </html>
--- index.php
+++ index.php
@@ -20,29 +20,25 @@
 
 <!DOCTYPE html>
 <html lang="de">
-
 <head>
     <title>Lea Berger | Umfrage</title>
-
+    
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <link rel="shortcut icon" type="image/x-icon" href="cubeicon.png">
-
-    <!-- Bootstrap -->
+    
+    <!-- Style -->
     <link href="//cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.0/paper/bootstrap.min.css" rel="stylesheet">
-
 </head>
-
 <body>
     <div class="row">
         <div class="col-lg-12 text-center">
             <legend>
                 <h1>Lea Berger - Maturitätsarbeit</h1>
             </legend>
-
         </div>
     </div>
-
+    
     <section id="infos">
         <div class="container">
             <div class="row">
@@ -123,7 +119,7 @@
             </div>
         </div>
     </section>
-
+    
     <footer class="footer">
         <div class="container text-center">
             <p class="text-muted">
@@ -131,12 +127,9 @@
             </p>
         </div>
     </footer>
-
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    
+    <!-- Scripts -->
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.11.1/jquery.min.js" type="text/javascript"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js" type="text/javascript"></script>
-
 </body>
-
 </html>
