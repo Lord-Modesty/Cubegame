6f54da6a163f400e089bad7dd727bc8c9fecabba
Dimitri Vranken <dimitri.vranken@hotmail.ch>
2014-12-10 22:21:58 +0100

    Refactored the database interactions

--- game_script.php
+++ game_script.php
@@ -1,34 +1,41 @@
 <?php
     session_start();
-    $runde = $_GET['runde'];
-    $guthaben = $_GET['guthaben'];
+    
+    // Get user input
+    $round = $_GET['runde'];
+    $credit = $_GET['guthaben'];
     $gesetzt = $_GET['gesetzt'];
-    $gewinn_zahl = $_GET['gewinn_zahl'];
-    $gewinn_farbe = $_GET['gewinn_farbe'];
-    $gewinn_gerade = $_GET['gewinn_gerade'];
-    $verloren = $_GET['verloren'];
-    $gewonnen = $_GET['gewonnen'];
-    $player_id = $_GET['player_id'];
-       
-       $db_connection = new mysqli('localhost', 'root', '', 'leabergermaturaarbeit') or die("Verbindung zur Datenbank konnte nicht hergestellt werden");
-       $db_connection ->set_charset('utf8');
-       
-       if (mysqli_connect_errno()) {
-           echo "Fehler aufgetreten: " . mysqli_connect_error();
-           exit();
-       } else {
-           
-           if ($stmt = $db_connection->prepare('INSERT INTO game (runde, guthaben,  gesetzt, gewinn_zahl, gewinn_farbe,gewinn_gerade, verloren,gewonnen, player_id) VALUES (?,?,?,?,?,?,?,?,?)')){
-               $stmt-> bind_param('iiiiiiiii', $runde, $guthaben,$gesetzt, $gewinn_zahl, $gewinn_farbe, $gewinn_gerade, $verloren, $gewonnen, $player_id);
-               $stmt->execute();
-               $stmt->close();
-               
-               $_SESSION['gewonnen'] = $gewonnen;
-               
-           }
-           else {
-               printf("Errormessage: %s\n", $db_connection->error);
-           }
-           
-       }    
+    $winningNumber = $_GET['gewinn_zahl'];
+    $winningColor = $_GET['gewinn_farbe'];
+    $winningEven = $_GET['gewinn_gerade'];
+    $amountLost = $_GET['verloren'];
+    $amountWon = $_GET['gewonnen'];
+    $playerId = $_GET['player_id'];
+    
+    // Create a database connection
+    $database = new mysqli('localhost', 'root', '', 'leabergermaturaarbeit') or
+      exit('Fehler: Verbindung zur Datenbank konnte nicht hergestellt werden');
+    $database->set_charset('utf8');
+    
+    if (mysqli_connect_errno()) {
+        exit('Fehler: ' . mysqli_connect_error());
+    }
+    
+    // Insert a new round result
+     if ($query = $database->prepare('INSERT INTO game ' .
+                                     '(runde, guthaben,  gesetzt, gewinn_zahl, gewinn_farbe,gewinn_gerade, verloren, gewonnen, player_id) ' .
+                                     'VALUES (?,?,?,?,?,?,?,?,?)')) {
+        $query->bind_param('iiiiiiiii', $round, $credit, $gesetzt, $winningNumber, $winningColor, $winningEven, $amountLost, $amountWon, $playerId);
+        $query->execute();
+        $query->close();
+    }
+    else {
+        exit('Fehler: ' . mysqli_connect_error());
+    }
+    
+    // Update the amount won
+    $_SESSION['gewonnen'] = $amountWon;
+    
+    // Close the database connection
+    $database->close();
 ?>
--- index_script.php
+++ index_script.php
@@ -1,48 +1,46 @@
 <?php
-
-        session_start();
-        $_SESSION['gameReached'] = true;
-		$vorname = $_POST['vorname'];
-		$name = $_POST['name'];
-        $klasse =  $_POST['klasse'];;
-		$geschlecht = $_POST['geschlecht'];
-        $age = $_POST['age'];
-		$gewicht = $_POST['gewicht'];
-        $groesse = $_POST['groesse'];
-		$hunger = $_POST['hunger'];
-		        
-        $db_connection = new mysqli('localhost', 'root', '', 'leabergermaturaarbeit') or die("Verbindung zur Datenbank konnte nicht hergestellt werden");
-        $db_connection ->set_charset('utf8');
-
-		if (mysqli_connect_errno()) {
-			echo "Fehler aufgetreten: " . mysqli_connect_error();
-			exit();
-		} else {
-                
-                if ($stmt = $db_connection->prepare('INSERT INTO players (vorname, name,  klasse, geschlecht, age,gewicht,groesse,hunger) VALUES (?,?,?,?,?,?,?,?)')) {
-                    
-                    $stmt-> bind_param('ssssssss', $vorname, $name, $klasse, $geschlecht, $age, $gewicht, $groesse, $hunger);
-                    $stmt->execute();
-                    $stmt->close();
-                    
-                    $querry = "SELECT player_id FROM players";
-                    $result = $db_connection->query($querry);
-
-                    if ($result->num_rows > 0) {
-                        while($row = $result->fetch_assoc()) {
-                        $_SESSION['player_id'] = $row["player_id"];
-                        }
-                    } else {
-                        echo "0 results";
-                    }
-                    $db_connection->close();
-
-                }
-                else {
-                    printf("Errormessage: %s\n", $db_connection->error);
-                }
-            
-			header('location: game.php');
-        }
+    session_start();
     
+    // Get user input
+    $vorname = $_POST['vorname'];
+    $name = $_POST['name'];
+    $klasse =  $_POST['klasse'];;
+    $geschlecht = $_POST['geschlecht'];
+    $age = $_POST['age'];
+    $gewicht = $_POST['gewicht'];
+    $groesse = $_POST['groesse'];
+    $hunger = $_POST['hunger'];
+    
+    // Create a database connection
+    $database = new mysqli('localhost', 'root', '', 'leabergermaturaarbeit') or
+      exit('Fehler: Verbindung zur Datenbank konnte nicht hergestellt werden');
+    $database->set_charset('utf8');
+    
+    if (mysqli_connect_errno()) {
+        exit('Fehler: ' . mysqli_connect_error());
+    }
+    
+    // Create a new player
+    if ($query = $database->prepare('INSERT INTO players ' . 
+                                    '(vorname, name, klasse, geschlecht, age, gewicht, groesse, hunger) ' .
+                                    'VALUES (?, ?, ?, ?, ?, ?, ?, ?)')) {
+        $query->bind_param('ssssssss', $vorname, $name, $klasse, $geschlecht, $age, $gewicht, $groesse, $hunger);
+        $query->execute();
+        $query->close();
+    }
+    else {
+        exit('Fehler: ' . mysqli_connect_error());
+    }
+    
+    // Retrieve the id of the newly created player
+    $playerId = $database->insert_id;
+    
+    // Close the database connection
+    $database->close();
+    
+    // Redirect to the next page
+    $_SESSION['player_id'] = $playerId;
+    $_SESSION['gameReached'] = true;
+    
+    header('location: game.php');
 ?>
